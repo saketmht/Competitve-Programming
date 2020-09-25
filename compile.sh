ba54a2b9b9cd297be2c720cbe1e747a529db32a8
Saket Mahto <54639179+saketmht@users.noreply.github.com>
2020-09-25 23:53:20 +0545

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,16 @@
-#standard
-g++ -std=c++14 -o $1 $1.cpp
-
-#compile with flags (faster - F8)
-#g++ -std=c++17 -Wshadow -Wall -o "%e" "%f" -O2 -Wno-unused-result
-
-#compile with additional flags (slower - F9)
-#g++ -std=c++17 -Wshadow -Wall -o "%e" "%f" -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG
+a=$2
+if [ $a == 0 ]
+then 
+  #standard
+	g++ -std=c++17 -o $1 $1.cpp
+elif [ $a == 1 ]
+then 
+	#compile with flags (faster - F8)
+	g++ -std=c++17 -Wshadow -Wall -o "$1" "$1.cpp" -O2 -Wno-unused-result
+elif [ $a == 2 ]
+then
+	#compile with additional flags (slower - F9)
+	g++ -std=c++17 -Wshadow -Wall -o "$1" "$1.cpp" -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG
+else
+	echo "Select proper flags...File not compiled"
+fi
